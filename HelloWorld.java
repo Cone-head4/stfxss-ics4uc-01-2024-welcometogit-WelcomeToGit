31f841ffd8b6207bf0b84da4b7df76d6b8bb0c7e
Cone-head4 <kmtuiza@gmail.com>
2025-04-17 13:45:44 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,15 +2,18 @@ import java.util.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // Creat a scanner to read
+        // Create a scanner to read
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = sc.next();
         System.out.println("Hello "+name);
+        System.out.println("Another Name: ");
+        String name2 = sc.next();
+        System.out.println("Hello thy: "+name2);
         sc.close();   
 
         // Simple shortform example
-        int num = 4;
+        int num = 900;
         System.out.println(num);
         System.out.println(num++);
         System.out.println(++num);
